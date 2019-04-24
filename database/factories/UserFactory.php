c54126f796184c72396e8b859e03ea90f6f18cee
Indrasinh <ib@gocaution.ch>
2019-04-24 12:16:27 +0530

    Added seeder for users.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,12 +12,12 @@ use Faker\Generator as Faker;
 | model instances for testing / seeding your application's database.
 |
 */
-
 $factory->define(App\User::class, function (Faker $faker) {
+	static $emails = ['ib@gocaution.ch', 'sb@gocaution.ch', 'rk@gocaution.ch', 'ym@gocaution.ch'];
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'email' => array_shift($emails),
+        'password' => bcrypt('GoCrm@123'), // GoCrm@123
         'remember_token' => str_random(10),
     ];
 });
