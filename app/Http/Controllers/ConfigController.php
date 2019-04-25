a8907b63043dc5d50d8d31f35f15adb16709b8fe
Indrasinh <ib@gocaution.ch>
2019-04-25 17:36:27 +0530

    Added Config APIs.

---
+++ app/Http/Controllers/ConfigController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Config;
+use App\Http\Resources\ConfigResource;
+use Illuminate\Http\Request;
+
+class ConfigController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return ConfigResource::collection(Config::paginate(config('pagination.items_per_page')));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'key' => 'required|alpha_dash|unique:configs,key',
+            'value' => 'required'        
+        ]);
+
+        return new ConfigResource(Config::create($request->all()));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Config  $config
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Config $config)
+    {
+        return new ConfigResource($config);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Config  $config
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Config $config)
+    {
+        $request->validate([
+            'key' => 'required|alpha_dash|unique:configs,key,' . $config->id,
+            'value' => 'required'
+        ]);
+
+        return response()->json([
+            "api_status" => $config->update($request->all()),
+            "data" => $config
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Config  $config
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Config $config)
+    {
+        return response()->json([
+            "api_status" => $config->delete()
+        ], 200);
+    }
+}
