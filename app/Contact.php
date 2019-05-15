bf8b5d452de1a21a3f5ce833c438ae771715edcc
Indrasinh <ib@gocaution.ch>
2019-05-15 11:29:38 +0530

    Status change from contact list view.

--- app/Contact.php
+++ app/Contact.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
 
 class Contact extends Model
 {
@@ -20,4 +21,61 @@ class Contact extends Model
     {
         return $this->hasMany('App\Address')->select(array('id', 'is_primary', 'address', 'zip', 'city'));
     }
+
+    public function offers()
+    {
+        return $this->hasMany('App\ContactOffer');
+    }
+
+    public function preconfirmations()
+    {
+        return $this->hasMany('App\ContactPreconfirm');
+    }
+
+    function change_status()
+    {
+        $old_status = $this->status;
+        $status = request()->status;
+
+        if(in_array($old_status, array('offer','pre_confirmation_sent')) && !in_array($status, array('offer','pre_confirmation_sent'))){
+            
+            $directory_kundendatenbank = config('app.customer_data');
+            if(!Storage::disk('public')->exists($directory_kundendatenbank)){
+                Storage::disk('public')->makeDirectory($directory_kundendatenbank);
+            }
+
+            $directory_offer = 'offer';
+            if(!Storage::disk('public')->exists($directory_kundendatenbank.DS.$directory_offer)){
+                Storage::disk('public')->makeDirectory($directory_kundendatenbank.DS.$directory_offer);
+            }
+
+            $directory_preconfirmation = 'preconfirmation';
+            if(!Storage::disk('public')->exists($directory_kundendatenbank.DS.$directory_preconfirmation)){
+                Storage::disk('public')->makeDirectory($directory_kundendatenbank.DS.$directory_preconfirmation);
+            }
+
+            $offer = $directory_kundendatenbank.DS.$directory_offer.DS.$this->id;
+            if(Storage::disk('public')->exists($offer)){
+                Storage::disk('public')->delete($offer);
+                foreach($this->offers as $offer){
+                    $offer->delete();
+                }
+            }
+
+            $preconfirmation = $directory_kundendatenbank.DS.$directory_preconfirmation.DS.$this->id;
+            if(Storage::disk('public')->exists($preconfirmation)){
+                Storage::disk('public')->delete($preconfirmation);
+
+                foreach($this->preconfirmations as $preconfirmation){
+                    $preconfirmation->delete();
+                }
+            }
+        }
+        $this->status = request()->status;
+        $this->save();
+        // @TODO Activity Log
+        //$dispatcher->trigger('onChangeContactStatus',array($cid, $status));
+        return true;
+    }
+
 }
