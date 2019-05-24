a93b53af4929f2bee25682698b2e12b17e152c7b
Rajesh Kaswala <rk@gocaution.ch>
2019-05-24 14:23:06 +0000

    Kernel.php edited online with Bitbucket

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,6 +67,10 @@ class Kernel extends HttpKernel
         'localization' => \App\Http\Middleware\Localization::class,
         //'cors' => \App\Http\Middleware\cors::class,
         'setLocale' => \App\Http\Middleware\SetLocale::class,
-        'Pagination' => \App\Http\Middleware\Pagination::class
+        'Pagination' => \App\Http\Middleware\Pagination::class,
+         'localize' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
+    		'localizationRedirect' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
+    		'localeSessionRedirect' => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
+        'localeViewPath' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class
     ];
 }
