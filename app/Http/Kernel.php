c1aebaf2920351494b1484719d3cd53dadb427dc
sandeep <sandeep.p@gocaution.ch>
2019-05-27 10:32:12 +0530

    adding myChanges in kernal

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,5 +68,9 @@ class Kernel extends HttpKernel
         //'cors' => \App\Http\Middleware\cors::class,
         'setLocale' => \App\Http\Middleware\SetLocale::class,
         'Pagination' => \App\Http\Middleware\Pagination::class,
+        'localize' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
+    		'localizationRedirect' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
+    		'localeSessionRedirect' => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
+        'localeViewPath' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class
     ];
 }
