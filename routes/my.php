903db170aba0c06e6bdcdf10c265408bedba5953
pg@gocautation.ch <webexpert4rv@gmail.com>
2019-05-31 17:54:06 +0530

    laralogin final changes for PR

--- routes/my.php
+++ routes/my.php
@@ -1,3 +1,22 @@
 <?php
 
-Route::get('/', 'ExampleController@index');
+Route::get('/home', function(){ return redirect( app()->getLocale().'/dashboard'); })->name('home');
+Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]], function()
+{
+    // echo LaravelLocalization::transRoute("routes.password_reset"); die();
+    Route::get('/', 'AuthLoginController@loginForm')->name('login');
+    Route::get(LaravelLocalization::transRoute("my.routes.FORGOT_PASSWORD_LINK"), 'AuthLoginController@showForgetPasswordForm');
+    Route::get(LaravelLocalization::transRoute("my.routes.PASSWORD_RESET").'/{token}','AuthLoginController@showResetPasswordForm');
+    Route::post(LaravelLocalization::transRoute("my.routes.LOGIN"),'AuthLoginController@login');
+    Route::post(LaravelLocalization::transRoute("my.routes.PASSWORD_EMAIL"), 'AuthLoginController@sendResetPasswordLinkEmail');
+    Route::post(LaravelLocalization::transRoute("my.routes.PASSWORD_UPDATE"), 'AuthLoginController@reset');
+
+    Route::middleware(['auth'])->group(function () {
+        Route::get('dashboard', function () { return view('My.dashboard.dashboardv1'); })->name('dashboard');
+        Route::post(LaravelLocalization::transRoute("my.routes.LOGOUT"), 'AuthLoginController@logout');
+    });
+    Route::any('/{any}',function(){
+        return  view('My.others.notFound');
+    })->where('any','.*')->name('any');
+});
+
