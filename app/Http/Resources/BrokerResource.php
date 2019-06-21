b8dc307acd9d2bbcd10fd483519bb0d3ed6e3d5a
Indrasinh Bihola <biholaindrasinh@gmail.com>
2019-06-21 16:34:03 +0530

    Broker search API and added more details.

--- app/Http/Resources/BrokerResource.php
+++ app/Http/Resources/BrokerResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
 
 class BrokerResource extends JsonResource
 {
@@ -15,8 +16,25 @@ class BrokerResource extends JsonResource
     public function toArray($request)
     {
         $data = parent::toArray($request);
+        $data['created_at_formatted'] = Carbon::parse($this->created_at)->format(config('crm.display_date_format'));
+        $this->policyStatusCount($data);
+        return $data;
+    }
+
+    private function policyStatusCount(&$data) 
+    {
         $policies = $this->policies;
         $data['count_policies'] = $policies->count();
-        return $data;
+        $policy_statuses = array_keys(getPolicyStatus());
+        foreach($policy_statuses as $policy_status) {
+            $count = $policies->where('status', $policy_status)->count();
+            if($count > 0) {
+                $count_class = [
+                    'count' => $count,
+                    'class' => render_status_class($policy_status)
+                ];
+                $data['count_policy_by_status'][$policy_status] = $count_class;
+            }
+        }
     }
 }
